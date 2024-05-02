091c28d1da7ef8def09b2862ea6a5e23a08fc93b
Вакуленко Алексей <ayv@pharmsklad.ru>
2024-05-02 09:51:20 +0400

    обновил jenkins 2.440.3

--- install/StartAgent.bat
+++ install/StartAgent.bat
@@ -1,2 +1,2 @@
 chcp 65001 
-java -jar agent.jar -jnlpUrl http://srv-jen2.gcfs.lan:8080/computer/work_3/jenkins-agent.jnlp -secret @secret.txt
+java -jar agent.jar -url http://srv-jen2.gcfs.lan:8080 -name w-test -secret @secret.txt
